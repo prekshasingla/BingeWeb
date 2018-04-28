83b4e28880e288883546aac7b9b66fe538673880
sumit kumar <sksumitkumar003@gmail.com>
2018-04-29 00:56:37 +0530

    modified order model

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -2,6 +2,6 @@ export class Order {
  public cart_value?: number;
  public dishes?: Object;
  public location_lat: number;
- public location_lang: number;
+ public location_long: number;
  public userId: string;
 }
