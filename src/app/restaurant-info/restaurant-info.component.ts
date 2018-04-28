9c334d2d2ca46af127a5d8f0fad30edf19605db5
sumit kumar <sksumitkumar003@gmail.com>
2018-04-29 01:00:06 +0530

    added latitude and longitude for location

--- src/app/restaurant-info/restaurant-info.component.ts
+++ src/app/restaurant-info/restaurant-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { OrdersService } from '../services/orders.service';
+import {} from '@types/googlemaps';
 
 @Component({
   selector: 'app-restaurant-info',
@@ -19,9 +20,18 @@ export class RestaurantInfoComponent implements OnInit {
   SaveInfo() {
     // console.log(this.Restaurant.restaurant_id, this.Email, this.desc, this.Address, this.FacebookLink, this.TwitterLink,
     //   this.Location, this.Contact_No, this.RestaurantName);
+    let location_lat ;
+    let location_long ;
+    const geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': this.Location}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        location_lat =  results[0].geometry.location.lat();
+        location_long = results[0].geometry.location.lng();
+      }
+    });
 
     this.Service.addRestaurantInfo(this.Restaurant.restaurant_id, this.Email, this.desc, this.Address, this.FacebookLink, this.TwitterLink,
-      this.Location, this.Contact_No, this.RestaurantName);
+      location_lat, location_long, this.Contact_No, this.RestaurantName);
   }
 
 }
