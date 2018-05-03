e2b2e4d24a5d9ae7bb89e0f8e824700cac8af6a0
sumit kumar <sksumitkumar003@gmail.com>
2018-05-04 00:47:15 +0530

    added remaining files

---
+++ Deploy/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PublicPath = path.join(__dirname , "/dist");
+
+app.use(express.static(PublicPath));
+
+app.get('/',function (req,res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.port || 4000,function () {
+  console.log('server has started');
+});
