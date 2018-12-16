e0ecefc8f459ca2ccd4804881c54ae83f7ed7959
samyak-branch <samyakb50@gmail.com>
2018-12-16 12:40:53 +0530

    work till 15.12.18-4

--- src/app/Routing/routing.module.ts
+++ src/app/Routing/routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes,RouterModule} from '@angular/router';
+import { SignupComponent } from '../signup/signup.component';
+import { LoginComponent } from '../login/login.component';
 import { LoginNewComponent } from '../loginNew/loginNew.component';
-
+import { OrderDetailComponent } from '../order-detail/order-detail.component';
+import { MenuItemComponent } from '../menu-item/menu-item.component';
+import { TableBookingComponent } from '../table-booking/table-booking.component';
+import { RestaurantInfoComponent } from '../restaurant-info/restaurant-info.component';
 const route:Routes=[
-    {path:'login',component:LoginNewComponent}
+  {path:'login',component:LoginNewComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'orders',component:OrderDetailComponent},
+  {path:'menu',component:MenuItemComponent},
+  {path:'tablebooking',component:TableBookingComponent},
+  {path:'restaurantinfo',component:RestaurantInfoComponent}
 ]
 @NgModule({
     imports: [
